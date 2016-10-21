240ef12197c736318f68f1ade217eb6f517048be
Muah <muah003@gmail.com>
2016-10-21 19:10:52 +0200

    init

---
+++ Commands/MysqlToMongo.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\DB;
+
+class MysqlToMongo extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'mongo:migrate';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'migrate mysql data to mongodb, also check (http://wp.me/p4DYee-9Q)';
+
+    /**
+     * Create a new command instance.
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $mongo_connection_name = $this->ask('What Is The Mongodb Connection Name ? ex.mongodb');
+        $remove_id_column      = $this->confirm('Do You Wish To Keep The Mysql Id Column ?');
+
+        // get all table names
+        $tables = DB::connection('mysql')->select('SHOW TABLES');
+
+        foreach ($tables as $one) {
+
+            // extract the name
+            $name = $one->Tables_in_homestead;
+
+            // get all the table data
+            $sql = DB::connection('mysql')->table($name)->get()->toArray();
+
+            // insert data to mongodb one by one
+            foreach ($sql as $item) {
+
+                // turn into array
+                $arr = (array) $item;
+
+                // remove the id column
+                if (!$remove_id_column) {
+                    $cln = array_shift($arr);
+                }
+
+                DB::connection($mongo_connection_name)->table($name)->insert($arr);
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # MysqlToMongoDb
 this is a simple console cmnd to migrate data from mysql to mongodb
+
+# PreRequisite
+- install https://moloquent.github.io/master/#moloquent
+- if you havent installed mongodb yet, check http://wp.me/p4DYee-9Q
+
+
+# Usage
+1- copy the `Commands/MysqlToMongo.php` file to `app/Console/Commands/MysqlToMongo.php`
+
+2- add the below to `app/Console/Kernel.php`
+
+```php
+protected $commands = [
+    // ...
+    Commands\MysqlToMongo::class,
+];
+```
+3- from the root of your project run `composer dump-autoload`
+
+4- run it by `php artisan mongo:migrate`
